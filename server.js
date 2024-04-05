be798c20df9151c4645bb32a82d07c119b9e6f7d
gautham <desertncamel@gmail.com>
2024-04-05 13:19:18 +0530

    [server]ver 1.0

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
