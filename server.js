cea228dccdf039f767f52db2fdec979e9c15e936
gautham <desertncamel@gmail.com>
2024-04-05 13:47:35 +0530

    development server patch

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
