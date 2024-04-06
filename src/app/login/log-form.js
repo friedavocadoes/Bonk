ecdc7086a0ea18019e472b50e059dc143296788d
gautham <desertncamel@gmail.com>
2024-04-06 13:14:39 +0530

    deletion

--- src/app/login/log-form.js
+++
@@ -1,9 +0,0 @@
-var modal = document.getElementById('modal-wrapper');
-function opener () {
-    document.getElementById('modal-wrapper').style.display='flex';
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
